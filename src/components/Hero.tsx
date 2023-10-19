fd28e5703f8dfab701071c501cbf78db681b1f7b
blazorin <alberto@darksnakegames.com>
2023-10-19 13:18:55 +0200

    Cambios

---
+++ src/components/Hero.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import exp from 'node:constants';
+
+const Hero = (props: any) => {
+	return (
+		<section className={'bg-color-main opacity-75 wide:padding-r padding-b'}>
+			<div>d</div>
+		</section>
+	);
+};
+
+export default Hero;
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,17 +1,35 @@
-import Logo from './svg/Logo';
+//import Logo from './svg/Logo';
+import LogoLV from '../assets/images/logo-vectoriel-louvre.jpg';
 
 const NavBar = (props: any) => {
+	const buttonColor: string = '#1b76de';
+
 	return (
 		<nav className={'bg-color-main opacity-75 p-7 w-full rounded-b-2xl'}>
 			<div className={'flex'}>
 				{/* Logo */}
-				<div className={'justify-start logo'}>
-					<Logo width={300} height={300} style={{ maxHeight: '40px', width: 'auto' }} />
+				<div className={'flex justify-start logo space-x-5 items-center cursor-zoom-in'}>
+					{/*<Logo width={300} height={300} style={{ maxHeight: '40px', width: 'auto' }} />*/}
+
+					<img
+						src={LogoLV}
+						alt={'Louvre'}
+						className={'flex-grow-0'}
+						style={{ height: '45px' }}></img>
+
+					<p className={'text-3xl font-black'}>Gallery</p>
 				</div>
 
-				<div className={'flex justify-end flex-1 space-x-4'}>
-					<button>Login</button>
-					<button>Apply</button>
+				<div className={'flex justify-end flex-1 space-x-4 nav-buttons'}>
+					<button
+						className={`bg-blue-200 text-md rounded-md font-extrabold px-5 py-2 hover:`}>
+						Login
+					</button>
+
+					<button
+						className={`bg-blue-600 text-sm rounded-md font-extrabold px-5 py-2 !text-white`}>
+						Apply
+					</button>
 				</div>
 			</div>
 		</nav>
